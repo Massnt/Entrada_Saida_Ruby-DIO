dd3682d17290ff2645daca1772cd0f6dd73a87c5
Mateus S.S <89607545+Massnt@users.noreply.github.com>
2023-09-10 19:51:44 -0300

    Add files via upload

---
+++ Entrada_Saida.rb
@@ -0,0 +1,10 @@
+puts "Digite seu nome:"
+nome = gets.chomp
+
+puts "Digite seu sobrenome:"
+s_nome = gets.chomp
+
+puts "Digite sua idade:"
+idade = gets.chomp.to_i
+
+puts "#{nome} #{s_nome} #{idade} anos"
